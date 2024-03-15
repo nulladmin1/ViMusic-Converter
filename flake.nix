8a20a64576e268004333e2f107c70e98ee2dfa95
nulladmin1 <shrey.deogade@protonmail.com>
2024-03-15 18:56:07 -0400

    Using flakes instead of nix-shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "ViMusic-Converter Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = 
+      pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            # Python packages
+            python311
+            python311Packages.python-dotenv
+            python311Packages.requests
+            python311Packages.spotipy
+            python311Packages.setuptools
+          ];
+        };
+  };
+}
